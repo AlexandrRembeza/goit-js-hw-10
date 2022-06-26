fda3c8877f7582ae8b2fa05c9302d5753032634f
AlexandrRembeza <99494287+AlexandrRembeza@users.noreply.github.com>
2022-06-27 02:16:51 +0300

    сделал дз

--- src/index.js
+++ src/index.js
@@ -1,3 +1,125 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import debounce from 'lodash.debounce';
+
+const refs = {
+  inputEL: document.querySelector('#search-box'),
+  listCountriesEL: document.querySelector('.country-list'),
+  oneCountryEL: document.querySelector('.country-info'),
+};
 
 const DEBOUNCE_DELAY = 300;
+let currentCountry = '';
+
+refs.inputEL.addEventListener(
+  'input',
+  debounce(findCountryAndRenderMarkup, DEBOUNCE_DELAY)
+);
+
+function findCountryAndRenderMarkup(e) {
+  const userInputValue = e.target.value.trim();
+
+  if (userInputValue === '') {
+    clearCountryDivAndCountriesList();
+    return;
+  }
+
+  fetchCountries(userInputValue)
+    .then(resolve => {
+      if (resolve.length > 10) {
+        clearCountryDivAndCountriesList();
+        showInfoMessage(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+
+      if (resolve.length > 1) {
+        renderCountriesMarkup(resolve);
+      } else if (resolve.length === 1) {
+        renderOneCountryMarkup(resolve);
+      }
+    })
+    .catch(showErrorMessage);
+}
+
+function renderOneCountryMarkup(country) {
+  if (country[0].name.common === currentCountry) {
+    return;
+  }
+
+  clearCountryDivAndCountriesList();
+
+  let oneCountryMarkup = country
+    .map(
+      ({
+        flags: { svg },
+        name: { common },
+        capital,
+        languages,
+        population,
+      }) => {
+        const languagesStr = Object.values(languages).join(', ');
+        currentCountry = common;
+
+        return `<p>
+                <span><img src="${svg}" alt="flag" width="40px" height="auto"></span>
+                <span class="country-name">${common}</span>
+                </p>
+                <p><span class="span-text">Capital:</span> ${capital}</p>
+                <p><span class="span-text">Population:</span> ${population}</p>
+                <p><span class="span-text">Languages:</span> ${languagesStr}</p>`;
+      }
+    )
+    .join('');
+
+  refs.oneCountryEL.insertAdjacentHTML('beforeend', oneCountryMarkup);
+}
+
+function renderCountriesMarkup(countries) {
+  clearCountryDivAndCountriesList();
+  currentCountry = '';
+
+  let countriesItemsMarkup = countries
+    .map(({ flags: { svg }, name: { common } }) => {
+      return `<li class="list-items">
+                 <div><img src="${svg}" alt="flag" width="40px" height="auto"></div> 
+                 <div class="item-text">${common}</div>
+              </li>`;
+    })
+    .join('');
+
+  refs.listCountriesEL.insertAdjacentHTML('beforeend', countriesItemsMarkup);
+}
+
+function clearCountryDivAndCountriesList() {
+  refs.listCountriesEL.innerHTML = '';
+  refs.oneCountryEL.innerHTML = '';
+}
+
+function showInfoMessage(message) {
+  return Notiflix.Notify.info(`${message}`, {
+    width: '400px',
+    position: 'center-top',
+    clickToClose: true,
+    svgSize: '120px',
+    fontSize: '18px',
+    timeout: 2000,
+  });
+}
+
+function showErrorMessage() {
+  clearCountryDivAndCountriesList();
+  currentCountry = '';
+
+  return Notiflix.Notify.failure(`Oops, there is no country with that name`, {
+    width: '400px',
+    position: 'center-top',
+    clickToClose: true,
+    svgSize: '120px',
+    fontSize: '18px',
+    timeout: 2000,
+  });
+}
